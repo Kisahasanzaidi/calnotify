8e14a8f01d260bba04861a5049882e893a7c536f
Kisa Hasan Zaidi <kisahasanzaidi@gmail.com>
2025-08-18 19:08:15 +0530

    Initial commit - project setup

---
+++ backend/src/main/java/com/kisa/calnotify/CalnotifyApplication.java
@@ -0,0 +1,13 @@
+package com.kisa.calnotify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalnotifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalnotifyApplication.class, args);
+	}
+
+}
