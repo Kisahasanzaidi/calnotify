ea27bac99f5e4fbd96b453f1e72fb83fb2d6953b
Kisa Hasan Zaidi <kisahasanzaidi@gmail.com>
2025-09-10 23:32:16 +0530

    check variable

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -4,6 +4,7 @@ import axios from "axios";
 const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL, 
 });
+console.log("API URL:", import.meta.env.VITE_API_URL);
 
 API.interceptors.request.use(
   (config) => {
